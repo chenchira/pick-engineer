812c7653bf98a6cd353938f2f5c169e648f79569
chenchira <devil-love-fay@hotmail.com>
2014-10-29 23:17:38 +0700

    Update picker.rb

--- picker.rb
+++ picker.rb
@@ -1,8 +1,14 @@
 def random_name( name )
 
 	content = File.readlines name
-	return content[ rand( content.size ) ]
-
+	rand_name = content[ rand( content.size ) ]
+	rn = rand_name.split(" ")
+	if rn.size > 1
+		new = rn[1]
+		return rn[0]+" "+new[0]
+	else
+		return rand_name
+	end
 end
 
 puts random_name( 'engineers.txt' )
